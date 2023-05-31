b1b7748ae2bcd39e6e9ff969c035589c70bceee9
sanday <sandayazisp12@gmail.com>
2023-05-31 14:10:31 +0700

    Feat: create footer landing page and styling
    Landing page layout

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,180 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Box, Container, Typography } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+
+export default function Footer() {
+    return(
+        <Box
+            sx={{
+                width: '100%',
+                position: 'relative',
+                height: '100%',
+                bgcolor: '#25D366',
+                mt: 15,
+                py: 4,
+
+            }}
+        >
+            <Container
+                sx={{
+                    width: '100%',
+                    maxWidth: {xs: '100%', md: '90%'},
+                    position: 'relative',
+                    
+                }}
+            >
+                <Grid container spacing={2}
+                    sx={{
+                        display: 'flex',
+                        width: '100%',
+                        height: '100%',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        fontSize: '1rem',
+                        fontWeight: '500',
+                        color: 'common.white',
+                    }}
+                >
+                    <Grid xs={12} md={6} order={{xs: 2, md: 1}}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection: {xs: 'column', md: 'row'},
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    display: {xs: 'none', md: 'block'},
+                                }}
+                            >
+                                <img
+                                    src='/Assets/Mask Group 8@2x.png'
+                                    alt='Wappin'
+                                    loading="lazy"
+                                    width='80px'
+                                />
+                            </Box>
+                            <Typography variant="body1" component='p'
+                                sx={{
+                                    ml: {xs: 0, md: 2},
+                                    mt: {xs: 2, md: 0},
+                                    textAlign: {xs: 'center', md: 'left'},
+                                    fontSize: {xs: '0.85rem', md: '1rem'},
+                                }}
+                            >
+                                &copy; 2019 Copyright by Wappin. All Right Reserved
+                            </Typography>
+                        </Box>
+                    </Grid>
+
+                    <Grid xs={12} md={6} order={{xs: 1, md: 2}}
+                        sx={{
+                            display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                zIndex: 1,
+                        }}
+                    >
+                            <Box
+                                sx={{
+                                position: 'relative',
+                                    width: '100%',
+                                    height: '100%',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    fontSize: '1rem',
+                                    flexDirection: {xs: 'column', md: 'row'},
+                                
+                                }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: {xs: 'block', md: 'none'},
+                                            bgcolor: 'common.white',
+                                            padding: '10px',
+                                            borderRadius: '8px',
+                                        }}
+                                        >
+                                            <img
+                                                src='/Assets/02_@2x.png'
+                                                alt='Wappin'
+                                                loading="lazy"
+                                                width='180px'
+                                            />
+                                        </Box>
+                                    <Typography variant="body1" component='p'
+                                        sx={{
+                                            mr: {xs: 0, md: 2},
+                                            borderBottom: {xs: '1px solid #fff', md: 'none'},
+                                            width: {xs: '100%', md: 'auto'},
+                                            textAlign: {xs: 'center', md: 'left'},
+                                            mb: {xs: 2, md: 0},
+                                            py: {xs: 2, md: 0},
+                                        }}
+                                    >
+                                        www.spesolution.com
+                                    </Typography>
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                        }}
+                                    >
+                                        <Typography variant='body1' component='a' href='#sdf'
+                                            sx={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                color: 'common.white',
+                                                mx: {xs: 0, md: 3},
+                                                mr: {xs: 3,},
+                                            }}
+                                        >
+                                            <FacebookIcon fontSize='large'  />
+                                        </Typography>
+                                        <Typography variant='body1' component='a' href='#sdf'
+                                            sx={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                color: 'common.white',
+                                            }}
+                                        >
+                                            <InstagramIcon fontSize='large' />
+                                        </Typography>
+                                        <Typography variant='body1' component='a' href='#sdf'
+                                            sx={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                color: 'common.white',
+                                                mx: 3,
+                                            }}
+                                        >
+                                            <TwitterIcon fontSize='large' />
+                                        </Typography>
+                                        <Typography variant='body1' component='a' href='#sdf'
+                                            sx={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                color: 'common.white',
+                                            }}
+                                        >
+                                            <GoogleIcon fontSize='large' />
+                                        </Typography>
+                                    </Box>
+                            </Box>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    )
+};
